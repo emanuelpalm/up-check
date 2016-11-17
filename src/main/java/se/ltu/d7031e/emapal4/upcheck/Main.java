9f6920444e1f4401ec4b2ac7e8532641fc6edd06
Emanuel Palm <emanuelpalm@gmail.com>
2016-11-17 12:06:29 +0100

    Configure gradle

---
+++ src/main/java/se/ltu/d7031e/emapal4/upcheck/Main.java
@@ -0,0 +1,17 @@
+package se.ltu.d7031e.emapal4.upcheck;
+
+/**
+ * Application main class.
+ *
+ * Only contains main function.
+ */
+public class Main {
+    /**
+     * Application main function.
+     *
+     * @param args Provided application command line arguments.
+     */
+    public static void main(final String[] args) throws Exception {
+        System.out.println("UpCheck");
+    }
+}
