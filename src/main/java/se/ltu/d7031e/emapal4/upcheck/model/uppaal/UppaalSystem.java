6a3eb9660e8cba9fb11013ed3df8ad9564e37992
Emanuel Palm <emanuelpalm@gmail.com>
2016-11-23 21:02:07 +0100

    Add empty UppaalProxy class

---
+++ src/main/java/se/ltu/d7031e/emapal4/upcheck/model/uppaal/UppaalSystem.java
@@ -0,0 +1,14 @@
+package se.ltu.d7031e.emapal4.upcheck.model.uppaal;
+
+import se.ltu.d7031e.emapal4.upcheck.util.DynamicObject;
+
+/**
+ * Created by emanuel on 11/23/16.
+ */
+public class UppaalSystem {
+    private final DynamicObject document;
+
+    UppaalSystem(final DynamicObject document) {
+        this.document = document;
+    }
+}
